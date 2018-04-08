4596a3a1802bbb50b151b9e330eb4f591c4f2521
juansmolano <juansmolano@gmail.com>
2018-04-08 11:13:03 -0500

    WORK IN PROGRESS: JWT supported on HTTP an WS, JWT on WS when using GraphiQl is missing

--- api/gateway/graphql/sample/resolvers.js
+++ api/gateway/graphql/sample/resolvers.js
@@ -1,11 +1,29 @@
+const withFilter = require('graphql-subscriptions').withFilter;
+const PubSub = require('graphql-subscriptions').PubSub;
+const pubsub = new PubSub();
+
 module.exports = {
   Query: {
     author(_, { firstName, lastName }) {
-      return { id: 1, firstName, lastName };
+      return { id: '1233', firstName, lastName };
     },
   },
   Mutation: {
-    createAuthor: (root, args) => { return { id: 3, firstName: args.firstName, lastName: args.lastName }; },
+    createAuthor: (root, args, context) => {
+      const authorAdded = { id: Math.random(), firstName: args.firstName, lastName: args.lastName };
+      pubsub.publish('authorAdded', {authorAdded});
+      return authorAdded;
+    },
+  },
+  Subscription: {
+    authorAddedFiltered: {
+      subscribe: withFilter(() => pubsub.asyncIterator('authorAdded'), (payload, variables) => {
+        return payload.authorAdded.lastName === variables.lastName;
+      }),
+    },
+    authorAdded: {
+      subscribe: () => pubsub.asyncIterator('authorAdded')
+    }
   },
 }
 
--- api/gateway/server.js
+++ api/gateway/server.js
@@ -14,26 +14,107 @@ const path = require('path');
 const mergeGraphqlSchemas = require('merge-graphql-schemas');
 const fileLoader = mergeGraphqlSchemas.fileLoader;
 const mergeTypes = mergeGraphqlSchemas.mergeTypes;
-const graphql = require('./graphql/index');
+const gqlSchema = require('./graphql/index');
 const broker = require('./broker/index');
+const cors = require('cors');
+const graphql = require('graphql');
+const execute = graphql.execute;
+const subscribe = graphql.subscribe;
+const http = require('http');
+const SubscriptionServer = require('subscriptions-transport-ws').SubscriptionServer;
+const jwt = require('express-jwt');
 
 //Service Port
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.GRAPHQL_END_POINT_PORT || 3000;
 //graphql types compendium
-const typeDefs = graphql.types;
+const typeDefs = gqlSchema.types;
 //graphql resolvers compendium
-const resolvers = graphql.resolvers;
+const resolvers = gqlSchema.resolvers;
 //graphql schema = join types & resolvers
 const schema = graphqlTools.makeExecutableSchema({ typeDefs, resolvers });
 
-const context = {
-    broker
-};
 
 //Express Server
-const app = express();
+const server = express();
 // bodyParser is needed just for POST.
-app.use('/graphql', bodyParser.json(), graphqlExpress({ schema, context }));
-app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+server.use(cors());
 
-app.listen(PORT);
+// server.use(
+//     process.env.GRAPHQL_HTTP_END_POINT,
+//     bodyParser.json(),
+//     graphqlExpress({ schema, context }));
+
+
+server.use(
+    process.env.GRAPHQL_HTTP_END_POINT,
+    bodyParser.json(),
+    jwt({
+        secret: process.env.JWT_PUBLIC_KEY.replace(/\\n/g, '\n'),
+        requestProperty: 'authToken',
+        credentialsRequired: true,
+        algorithms: ['RS256']
+    }), graphqlExpress(req => ({
+        schema,
+        context: {
+            authToken: req.authToken,
+            broker
+        },
+    })));
+
+server.use(process.env.GRAPHIQL_HTTP_END_POINT, graphiqlExpress(
+    {
+        endpointURL: process.env.GRAPHQL_HTTP_END_POINT,
+        subscriptionsEndpoint: `ws://${process.env.GRAPHQL_END_POINT_HOST}:${process.env.GRAPHQL_END_POINT_PORT}${process.env.GRAPHQL_WS_END_POINT}`
+    }
+));
+
+
+// Wrap the Express server
+const ws = http.createServer(server);
+ws.listen(PORT, () => {
+    new SubscriptionServer(
+        {
+            execute,
+            subscribe,
+            schema,
+            onConnect: (connectionParams, webSocket, context) => {
+                console.log(`GraphQL_WS.onConnect: ${JSON.stringify({ connectionParams })}`);
+                if (!connectionParams.authToken) {
+                    // return validateToken(connectionParams.authToken)
+                    //     .then(findUser(connectionParams.authToken))
+                    //     .then((user) => {
+                    //         return {
+                    //             currentUser: user,
+                    //         };
+                    //     });
+                    console.log('Missing auth token!');
+                    console.log(`para,s: :${JSON.stringify(connectionParams,null,2)}`);
+                    throw new Error('Missing auth token!');
+                }else{
+                    console.log(`auth token: :${connectionParams.auth}`);
+                }
+
+                
+            },
+            onOperation: (message, params, webSocket) => {
+                console.log(`GraphQL_WS.onOperation: ${JSON.stringify({ message, params })}`);
+                return message;
+            },
+            onOperationDone: webSocket => {
+                console.log(`GraphQL_WS.onOperationDone ${JSON.stringify({})}`);
+            },
+            onDisconnect: (webSocket, context) => {
+                console.log(`GraphQL_WS.onDisconnect: ${JSON.stringify({})}`);
+            },
+        },
+        {
+            server: ws,
+            path: process.env.GRAPHQL_WS_END_POINT,
+        });
+    console.log(`Apollo Server is now running on http://localhost:${PORT}`);
+    console.log(`HTTP END POINT: http://${process.env.GRAPHQL_END_POINT_HOST}:${process.env.GRAPHQL_END_POINT_PORT}${process.env.GRAPHQL_HTTP_END_POINT}`);
+    console.log(`WEBSOCKET END POINT: ws://${process.env.GRAPHQL_END_POINT_HOST}:${process.env.GRAPHQL_END_POINT_PORT}${process.env.GRAPHQL_WS_END_POINT}`);
+    console.log(`GRAPHIQL PAGE: http://${process.env.GRAPHQL_END_POINT_HOST}:${process.env.GRAPHQL_END_POINT_PORT}${process.env.GRAPHIQL_HTTP_END_POINT}`);
+
+    // Set up the WebSocket for handling GraphQL subscriptions
+});
