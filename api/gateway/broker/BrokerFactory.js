ebe48c12ba26765e8145aa5289878063d11c3f11
Jaime Zapata <esteban.zapata@nebulae.com.co>
2018-05-09 19:43:17 -0500

    creates broker as a singleton

--- api/gateway/broker/BrokerFactory.js
+++ api/gateway/broker/BrokerFactory.js
@@ -8,16 +8,17 @@ class BrokerFactory {
 
     switch (BROKER_TYPE) {
       case 'PUBSUB':
+      
         const PubSubBroker = require('./PubSubBroker');
         this.broker = new PubSubBroker({
           replyTimeOut: process.env.REPLY_TIMEOUT || 2000,
           projectId: process.env.GCLOUD_PROJECT_ID,
           gatewayRepliesTopic: process.env.GATEWAY_REPLIES_TOPIC,
-          gatewayRepliesTopicSubscription:
-            process.env.GATEWAY_REPLIES_TOPIC_SUBSCRIPTION,
+          gatewayRepliesTopicSubscription: process.env.GATEWAY_REPLIES_TOPIC_SUBSCRIPTION,
           gatewayEventsTopic: process.env.GATEWAY_EVENTS_TOPIC,
-          gatewayEventsTopicSubscription:
-            process.env.GATEWAY_EVENTS_TOPIC_SUBSCRIPTION
+          gatewayEventsTopicSubscription: process.env.GATEWAY_EVENTS_TOPIC_SUBSCRIPTION,
+          materializedViewTopic : process.env.GATEWAY_MATERIALIZED_VIEW_UPDATES_TOPIC,
+          materializedViewTopicSubscription : process.env.GATEWAY_MATERIALIZED_VIEW_UPDATES_TOPIC_SUBSCRIPTION,
         });
         break;
       case 'MQTT':
@@ -25,9 +26,10 @@ class BrokerFactory {
         this.broker = new MqttBroker({
           gatewayRepliesTopic: process.env.GATEWAY_REPLIES_TOPIC,
           gatewayEventsTopic: process.env.GATEWAY_EVENTS_TOPIC,
+          materializedViewTopic : process.env.GATEWAY_MATERIALIZED_VIEW_UPDATES_TOPIC,
           projectId: process.env.GCLOUD_PROJECT_ID,
           mqttServerUrl: process.env.MQTT_SERVER_URL,
-          replyTimeout: process.env.REPLY_TIMEOUT || 2000
+          replyTimeout: process.env.REPLY_TIMEOUT ||  2000
         });
         break;
     }
